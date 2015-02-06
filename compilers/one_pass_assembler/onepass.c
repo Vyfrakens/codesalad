651c0640ae8f2455755534c295dfdbf9c71fa43b
Kenny Jacob <johnjacobkenny@gmail.com>
2015-02-07 00:58:01 +0530

    onepass.c: Enable scanning through asm file.
    Makes function one_pass() which will traverse through the file and spit out debug information.

--- compilers/one_pass_assembler/onepass.c
+++ compilers/one_pass_assembler/onepass.c
@@ -1,7 +1,22 @@
 #include<stdio.h>
 
-main() {
+void one_pass() {
+	FILE *f = fopen("copy.asm", "r");
+	char label[10], mnemonic[10], operand[10];
+	while(!feof(f)) {
+		fscanf(f, "%s", label);
+		
+		if(strcmp(label, ".") == 0) {
+			while(fgetc(f) != '\n');
+			continue;
+		}
+		
+		fscanf(f, "%s%s", mnemonic, operand);
+		if(feof(f)) break;
+		printf("lab:%s mnem:%s op:%s\n", label, mnemonic, operand);
+	}
 	
+	fclose(f);
 }
 
 int get_opcode(char *ch) {
@@ -21,3 +36,8 @@ int get_opcode(char *ch) {
 	fclose(f);
 	return -1;
 }
+
+main() {
+	one_pass();	
+}
+
