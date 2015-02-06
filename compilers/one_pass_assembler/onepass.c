df60c1a16a715aea255aa02dc8e55cf367bf865a
Kenny Jacob <johnjacobkenny@gmail.com>
2015-02-07 00:09:56 +0530

    Add optab file

---
+++ compilers/one_pass_assembler/onepass.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+main() {
+	
+}
+
+int get_opcode(char *ch) {
+	FILE *f = fopen("optab", "r");
+	char mnemonic[10];
+	int opcode;
+	
+	while(!feof(f)) {
+		fscanf(f, "%s\t%x", mnemonic, &opcode);
+		if(feof(f)) break;
+		if(strcmp(mnemonic, ch) == 0) {
+			fclose(f);
+			return opcode;
+		}
+	}
+	
+	fclose(f);
+	return -1;
+}
