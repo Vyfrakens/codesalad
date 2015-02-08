c3dfc4dadcd658e7e4694c2d276792e13cb3669f
Kenny Jacob <johnjacobkenny@gmail.com>
2015-02-08 23:16:23 +0530

    palindrome_recursion.cpp: Clean up some code :)

--- algorithms/palindrome_recursion.cpp
+++ algorithms/palindrome_recursion.cpp
@@ -1,28 +1,27 @@
 #include<iostream>
 using namespace std;
 int palindrome(char A[],int i,int j){
-	int boolean;
-	if(j<i)
+	if(j < i)
 		return 1;
-	else
 	if(i==j && A[i]==A[j])
 		return 1;
-	else
 	if(A[i]!=A[j])
 		return 0;
 	else
-		return boolean=palindrome(A,i+1,j-1);
+		return palindrome(A,i+1,j-1);
 		
 }
 int main (int argc, char *argv[]){
 	char A[20];
-	int j,boolean,i=0;
-	cout<<"enter string\n";
+	int j;
+	cout<<"Enter a String\n";
 	cin.getline(A,20);
 	for(j=0;A[j]!='\0';j++);
-	j=j-1;
-	boolean=palindrome(A,i,j);
-	cout<<boolean;
+	if(palindrome(A,0,j-1) == 0)
+		cout<<"It's not a palindrome.";
+	else
+		cout<<"It's a palindrome.";
+	cout<<endl;
 	return 0;
 }
 
