7b815b3fe30bd1d8ece02d8f775c10a00b5b0fa3
Francis Johny <francjohny@gmail.com>
2015-03-14 14:48:23 +0530

    modified algo

--- compilers/parsers/FirstToBeFollowed.c
+++ compilers/parsers/FirstToBeFollowed.c
@@ -7,15 +7,15 @@
 //
 
 //TODO : FOLLOW()
-//Also FIRST is incomplete :P
 
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
 
-int m1, n, i, j = 3, z, flag = 0, old;
+int m1, n, i, j = 2, z, flag = 0, old;
 char a[10][10], g[10];
 void first(char);
+void check(char);
 
 int main()
 {
@@ -52,24 +52,40 @@ void first(char c)
     int k;
     if (!isupper(c)) {
         g[m1++] = c;
-        flag = 0;
     }
     
     for (k = 0; k < n; k++) {
         if (a[k][0] == c) {
             if (a[k][2] == '$') {
-                if (!flag) {
-                    g[m1++] = a[k][2];
-                } else {
-                    first(a[old][j++]);
-                }
+                g[m1++] = a[k][2];
             } else if (islower(a[k][2])) {
                 g[m1++] = a[k][2];
             } else {
-                flag = 1;
-                old = k;
                 first(a[k][2]);
+                for (j = 2; a[k][j+1] != '\0';) {
+                    j++;
+                    if(check('$')) {
+                        first(a[k][j]);
+                    }
+                }
             }
         }
     }
 }
+
+int check(char ch)
+{
+    int i;
+    for (i = 0; g[i] != '\0'; i++) {
+        if (g[i] == ch) {
+            g[i] = '!';
+            return 1;
+        }
+    }
+    return 0;
+}
+
+
+
+
+
